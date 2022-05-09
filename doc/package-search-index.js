e963e2be5505e106e1c49809cd8fb635bfc1d9ee
patriziorenelli <patriziore.pr@gmail.com>
2022-05-09 21:44:03 +0200

    Delete doc directory

--- doc/package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"it.uniroma1.textadv"},{"l":"it.uniroma1.textadv.collegamenti"},{"l":"it.uniroma1.textadv.eccezioni"},{"l":"it.uniroma1.textadv.oggetti"},{"l":"it.uniroma1.textadv.personaggi"}];updateSearchResults();
